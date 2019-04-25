5573486e5c5fb668cf97e11b14fcac59209c4dc7
matt <matt.bernardini@gmail.com>
2019-04-25 11:00:01 -0500

    Automated Upkeep

--- install_env.sh
+++ install_env.sh
@@ -3,8 +3,10 @@
 # -v is Verbose 
 # -p is create parents if needed
 # a+wt makes directory sticky so only owner of a file can delete
+export LFS=$HOME/
 (mkdir -v -p $LFS/sources && chmod -v a+wt $LFS/sources)
 wget http://www.linuxfromscratch.org/lfs/view/stable/wget-list > wget-list.txt
+
 wget --input-file=wget-list.txt --continue --directory-prefix=$LFS/sources
 (cd $LFS/sources && git clone https://github.com/openssl/openssl.git && cd openssl && ./config --prefix=$HOME && make && make test && make install)
 (cd $LFS/sources && git clone https://github.com/git/git && cd git && make configure && ./configure --prefix=$HOME && make install)
