859629d8931bc319ead7d3b4221ccb0c865e4c66
matt <matt.bernardini@gmail.com>
2019-05-13 17:00:02 -0500

    Automated Upkeep

--- lfs/install_env.py
+++ lfs/install_env.py
@@ -28,20 +28,20 @@ if __name__ == "__main__":
     LFS = os.path.expanduser('~/')
     lfs_sources = LFS + "sources/"
     # Section 3
-    # create_directory(LFS + "sources", stat.S_IRWXU + stat.S_ISVTX + stat.S_IWGRP + stat.S_IWOTH)
-    # wget_list_path = LFS + "sources/wget-list.txt"
-    # files = urllib.request.urlretrieve("http://www.linuxfromscratch.org/lfs/view/stable/wget-list", wget_list_path)
-    # with open(wget_list_path, 'r') as f:
-    #     for line in f:
-    #         print("Downloading " + line.strip() + "....")
-    #         urllib.request.urlretrieve(line, filename=LFS + "sources/" + line.split("/")[-1].strip())
+    create_directory(LFS + "sources", stat.S_IRWXU + stat.S_ISVTX + stat.S_IWGRP + stat.S_IWOTH)
+    wget_list_path = LFS + "sources/wget-list.txt"
+    files = urllib.request.urlretrieve("http://www.linuxfromscratch.org/lfs/view/stable/wget-list", wget_list_path)
+    with open(wget_list_path, 'r') as f:
+        for line in f:
+            print("Downloading " + line.strip() + "....")
+            urllib.request.urlretrieve(line, filename=LFS + "sources/" + line.split("/")[-1].strip())
 
     """
         Section 4
     """
-    # create_directory(LFS + "tools", None)
+    create_directory(LFS + "tools", None)
     # # From section 5.1
-    # os.system("mkdir -v " + LFS + "tools/lib && ln -sv lib " + LFS + "tools/lib64")
+    os.system("mkdir -v " + LFS + "tools/lib && ln -sv lib " + LFS + "tools/lib64")
 
     """
         Section 5
@@ -56,14 +56,14 @@ if __name__ == "__main__":
 
     # Binutils
     bin_utils = list_of_files[8]
-    # create_directory('.'.join(bin_utils.split(".")[:-2]))
-    # os.system("tar -xJf " + bin_utils + " --directory " + '.'.join(bin_utils.split(".")[:-2]) + " --strip-components=1")
-    # os.system("cd " + '.'.join(bin_utils.split(".")[:-2]) + " && mkdir -v build && cd build && " \
-    #     " ../configure "
-    #     "--prefix=" + LFS + "tools "
-    #     "--with-sysroot=" + LFS +
-    #     " --with-lib-path=" + LFS + "tools/lib"
-    #     "--disable-nls --disable-werror && " + make_command)
+    create_directory('.'.join(bin_utils.split(".")[:-2]))
+    os.system("tar -xJf " + bin_utils + " --directory " + '.'.join(bin_utils.split(".")[:-2]) + " --strip-components=1")
+    os.system("cd " + '.'.join(bin_utils.split(".")[:-2]) + " && mkdir -v build && cd build && " \
+        " ../configure "
+        "--prefix=" + LFS + "tools "
+        "--with-sysroot=" + LFS +
+        " --with-lib-path=" + LFS + "tools/lib"
+        "--disable-nls --disable-werror && " + make_command)
 
     # GCC Pass 1
 
@@ -72,34 +72,30 @@ if __name__ == "__main__":
     mpc = list_of_files[53]
     gmp = list_of_files[29]
 
-    # create_directory('.'.join(gcc.split(".")[:-2]))
-    # create_directory('.'.join(gcc.split(".")[:-2]) + "/build")
+    create_directory('.'.join(gcc.split(".")[:-2]))
+    create_directory('.'.join(gcc.split(".")[:-2]) + "/build")
 
-    # print("Untarring gcc...")
-    # os.system("tar -xJf " + gcc + " --directory " + '.'.join(gcc.split(".")[:-2]) + " --strip-components=1")
+    print("Untarring gcc...")
+    os.system("tar -xJf " + gcc + " --directory " + '.'.join(gcc.split(".")[:-2]) + " --strip-components=1")
 
-    # create_directory('.'.join(gcc.split(".")[:-2]) + "/mpfr")
-    # print("Untarring mpfr...")
-    # os.system("cd " + '.'.join(gcc.split(".")[:-2]) + " && " \
-    #     "tar -xJf " + mpfr + " --directory mpfr --strip-components=1")
+    create_directory('.'.join(gcc.split(".")[:-2]) + "/mpfr")
+    print("Untarring mpfr...")
+    os.system("cd " + '.'.join(gcc.split(".")[:-2]) + " && " \
+        "tar -xJf " + mpfr + " --directory mpfr --strip-components=1")
 
-    # create_directory('.'.join(gcc.split(".")[:-2]) + "/mpc")
-    # print("Untarring mpc...")
-    # os.system("cd " + '.'.join(gcc.split(".")[:-2]) + " && " \
-    #     "tar -xzf " + mpc + " --directory mpc --strip-components=1")
+    create_directory('.'.join(gcc.split(".")[:-2]) + "/mpc")
+    print("Untarring mpc...")
+    os.system("cd " + '.'.join(gcc.split(".")[:-2]) + " && " \
+        "tar -xzf " + mpc + " --directory mpc --strip-components=1")
 
-    # create_directory('.'.join(gcc.split(".")[:-2]) + "/gmp")
-    # print("Untarring gmp...")
-    # os.system("cd " + '.'.join(gcc.split(".")[:-2]) + " && " \
-    #     "tar -xJf " + gmp + " --directory gmp --strip-components=1")
+    create_directory('.'.join(gcc.split(".")[:-2]) + "/gmp")
+    print("Untarring gmp...")
+    os.system("cd " + '.'.join(gcc.split(".")[:-2]) + " && " \
+        "tar -xJf " + gmp + " --directory gmp --strip-components=1")
 
     print("Changing GCC's dynamic linker to use installed tools and changing to lib...")
-    print('.'.join(gcc.split(".")[:-2]) + "/config")
-    for file in os.listdir('.'.join(gcc.split(".")[:-2]) + "/config"):
-        print(file)
     os.system("cd " + '.'.join(gcc.split(".")[:-2]) + " && " \
         "bash " + '/'.join(os.path.realpath(__file__).split("/")[:-1]) + "/gcc_pass_one.sh")
-    exit()
 
     os.system("cd " + '.'.join(gcc.split(".")[:-2]) + "/build && " \
         "../configure "                                  \
